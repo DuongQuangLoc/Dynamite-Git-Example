f2a3dc79e41342df10162b4bf20691093ca843fa
dang ngoc khanh quang <khanhquang15@gmail.com>
2015-05-06 16:00:42 +0700

    no message

--- New/New/AppDelegate.m
+++ New/New/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "CheckVC.h"
 @interface AppDelegate ()
 
 @end
@@ -16,6 +16,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window =[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+    
+    CheckVC *checkVC = [[CheckVC alloc]init];
+    
+    UINavigationController *navi = [[UINavigationController alloc]initWithRootViewController:checkVC];
+    self.window.rootViewController= navi;
+
+    
     // Override point for customization after application launch.
     return YES;
 }
