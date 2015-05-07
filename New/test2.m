165d4586e1c5c04c43b70c15a10c107f62621567
Duong Quang Loc <duongquangloc.it.93@gmail.com>
2015-05-07 01:12:51 -0700

    test 2 LocBR

---
+++ New/test2.m
@@ -0,0 +1,37 @@
+//
+//  test2.m
+//  New
+//
+//  Created by Duong Quang Loc on 5/7/15.
+//  Copyright (c) 2015 dang ngoc khanh quang. All rights reserved.
+//
+
+#import "test2.h"
+
+@interface test2 ()
+
+@end
+
+@implementation test2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
